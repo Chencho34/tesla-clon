d62d5f3397dead0bc9f128a02efa4f4d10656093
Armando <armandocrescencio343@gmail.com>
2023-01-18 14:40:24 -0600

    Interactividad al burgerIcon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
-// import Header from './app/components/Header';
+import Header from './app/components/Header';
 import Home from './app/components/Home';
-
-
 
 function App() {
   return (
     <div className="App">
-      {/* <Header/> */}
+      <Header/>
       <Home/> 
     </div>
   );
