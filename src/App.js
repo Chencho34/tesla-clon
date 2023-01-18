c56031757817e1f6fc57229fdf04e5549eaffda1
Armando <armandocrescencio343@gmail.com>
2023-01-18 15:24:31 -0600

    ADD: carSlice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Header from './app/components/Header';
-import Home from './app/components/Home';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
